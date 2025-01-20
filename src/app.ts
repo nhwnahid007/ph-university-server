f0341151132196ef0801e7bd6f1b77343c013391
nhwnahid007 <nhwnahid@gmail.com>
2025-01-21 03:04:57 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response } from 'express'
+import cors from 'cors'
+
+const app = express()
+// const port = 3000
+
+//parsers
+
+app.use(express.json())
+app.use(cors())
+
+app.get(
+  '/',
+
+  (req: Request, res: Response) => {
+    let b
+    const a = 10
+    res.send(a)
+  },
+)
+
+export default app
